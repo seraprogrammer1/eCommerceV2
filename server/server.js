3eb4d9808207a92444f3859dd2fd01d3c3348bfd
seraprogrammer1 <165708692+seraprogrammer1@users.noreply.github.com>
2024-07-22 12:07:03 -0400

    update

--- server/server.js
+++ server/server.js
@@ -14,13 +14,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../client/", "dist")));
 app.use("/api", router);
 
-app.use(
-  "/images",
-  express.static(path.join(__dirname, "../client", "/public", "/images"))
-);
+app.use("/images", express.static(path.join(__dirname, "/dist", "/images")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "/dist", "index.html"));
 });
 
 router.get("/products", api.products);
